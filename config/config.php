e7ebec473be6a6e15cc982f635ee857bb6db7539
adiputra057 <150322564+adiputra057@users.noreply.github.com>
2026-01-31 00:33:11 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
 $port = getenv("DB_PORT");
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
   die("Koneksi gagal: " . $conn->connect_error);
