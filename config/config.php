50e7f59e13753a4d2e6781e346ca4eac7addb590
adiputra057 <150322564+adiputra057@users.noreply.github.com>
2026-01-31 03:18:14 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,10 +1,11 @@
 <?php
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = (int) getenv("MYSQLPORT");
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
+// Pastikan menyertakan $port agar tidak lari ke socket default
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
