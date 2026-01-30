15e2ff3f267393aa0846cc6fc2624c5a8389ad0c
adiputra057 <150322564+adiputra057@users.noreply.github.com>
2026-01-31 02:04:40 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
-$port = getenv("DB_PORT");
+$port = (int) getenv("DB_PORT");   // <-- penting!
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
